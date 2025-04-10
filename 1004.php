138d431b5d7391da2aae4a7d697bcf6baf2b5e3d
LUCASAMR23 <lucasrodriguessptvv@gmail.com>
2025-04-10 09:17:41 -0300

    Add files via upload

---
+++ 1004.php
@@ -0,0 +1,29 @@
+<?php
+ class carro{
+
+    public $marca;
+    public $modelo;
+    public $ano;
+    public $cor;
+    public $tabela;
+
+
+
+    public function_construct($marca, $modelo, $ano, $cor, $tabela)
+    $this->marca = $marca
+    $this->modelo = $modelo
+    $this->ano = $ano
+    $this->cor = $cor
+    $this->tabela = $tabela
+ }
+
+ public function exibirinformacoes(){
+    echo "Marca: "  . $this->marca . "\n";
+    echo "modelo: "  . $this->modelo . "\n";
+    echo "ano: "  . $this->ano . "\n";
+    echo "cor: "  . $this->cor . "\n";
+    echo "tabela: "  . $this->tablea . "\n";
+ }
+
+
+?>
